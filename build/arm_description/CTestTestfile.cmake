f3e635888bd4aafe037333490aff56ab2cef4392
Jorge Fernando Salgueiro Vargas <yorch.academico@gmail.com>
2026-06-08 01:09:07 -0400

    visualización_rviz

---
+++ build/arm_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yorch/Documentos/archivos_de_programas/robotica/robotic_arm_cell_holder/src/arm_description
+# Build directory: /home/yorch/Documentos/archivos_de_programas/robotica/robotic_arm_cell_holder/build/arm_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
