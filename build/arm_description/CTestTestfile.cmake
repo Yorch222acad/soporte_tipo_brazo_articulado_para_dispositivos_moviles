2d43f3d2e1f81efd22383d4eedac2bd305243a1d
Jorge Fernando Salgueiro Vargas <yorch.academico@gmail.com>
2026-06-08 01:14:28 -0400

    Fix: Ignore build, install, and log directories and remove them from index

--- build/arm_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yorch/Documentos/archivos_de_programas/robotica/robotic_arm_cell_holder/src/arm_description
-# Build directory: /home/yorch/Documentos/archivos_de_programas/robotica/robotic_arm_cell_holder/build/arm_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
